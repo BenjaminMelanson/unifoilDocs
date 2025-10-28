fda34813ad9d6753374cf1451e49c8a76df08a93
BenjaminMelanson <bmelanso@vols.utk.edu>
2025-10-27 20:10:01 -0400

    Updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UniFoil'
+copyright = 'UTK DyMDO Lab, 2025'
+author = 'Benjamin Melanson'
 
 release = '0.1'
 version = '0.1.0'
