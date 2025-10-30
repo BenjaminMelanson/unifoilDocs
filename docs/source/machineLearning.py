5b026ce966bebbced977b64b1e4e778fd61af537
BenjaminMelanson <bmelanso@vols.utk.edu>
2025-10-30 09:09:00 -0400

    Create machineLearning.py for ML documentation
    Added documentation for Machine Learning algorithm training using the UniFoil Dataset and TensorFlow.

---
+++ docs/source/machineLearning.py
@@ -0,0 +1,9 @@
+Machine Learning
+===
+
+.. autosummary::
+   :toctree: generated
+
+To test the UniFoil Dataset, a set of scripts were used to access the dataset and use the information for training a Machine Learning Algorithm.
+This algorithm was trained in TensorFlow on the Turbulent Airfoil Data.
+This section will include the documentation for this work and should serve as a starting point for ML Research.
