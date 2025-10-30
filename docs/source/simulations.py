a2177b3be0503793db786e89102d3ee7ebc87074
BenjaminMelanson <bmelanso@vols.utk.edu>
2025-10-30 09:11:11 -0400

    Add documentation for simulations in simulations.py
    Document the simulations using ADFlow and MACH-Aero.

---
+++ docs/source/simulations.py
@@ -0,0 +1,11 @@
+Simulations
+===
+
+.. autosummary::
+   :toctree: generated
+
+UniFoil used ADFlow and the MACH-Aero library to simulate the data.
+In the event that the data provided in our lab is insufficent for your needs, we have also included scripts to help generate more samples.
+You can customise the generation arguments to gather simulations with specific properties.
+It is important to note that ADFlow and its dependances are optimized for CPU work and not GPUs.
+It is recommended to use a high power CPU Cluster if available to speed up processing.
